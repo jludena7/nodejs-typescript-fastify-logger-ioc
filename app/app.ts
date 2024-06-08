5a7c2950aa5c340f88088584e0fa2703d5fe1348
jludena7 <jludena7@hotmail.com>
2024-06-08 18:05:25 -0500

    Update config format code

--- app/app.ts
+++ app/app.ts
@@ -1,29 +1,33 @@
 import "reflect-metadata";
-import fastify, {FastifyError, FastifyReply, FastifyRequest} from 'fastify';
+import fastify, { FastifyError, FastifyReply, FastifyRequest } from "fastify";
 import fastifyCors from "@fastify/cors";
-import {ERROR_404, ERROR_500} from "./src/common/messages/common.errors";
+import { ERROR_404, ERROR_500 } from "./src/common/messages/common.errors";
 import apiRoutes from "./src/routers";
-import {API, CORS} from "./src/common/constants/app.constants";
+import { API, CORS } from "./src/common/constants/app.constants";
 import appLogger from "./src/common/logger";
 
 const app = fastify();
 
 app.register(fastifyCors, {
-    origin: CORS.ALLOW,
-    methods: CORS.METHODS,
+	origin: CORS.ALLOW,
+	methods: CORS.METHODS,
 });
 
-app.register(apiRoutes, {prefix: API.VERSION_V1});
+app.register(apiRoutes, { prefix: API.VERSION_V1 });
 
 app.setNotFoundHandler((request: FastifyRequest, reply: FastifyReply): void => {
-    appLogger.info("setNotFoundHandler | url: ", request.url);
-    reply.status(ERROR_404.ROUTER_NOT_FOUND.statusCode).send(ERROR_404.ROUTER_NOT_FOUND);
+	appLogger.info("setNotFoundHandler | url: ", request.url);
+	reply
+		.status(ERROR_404.ROUTER_NOT_FOUND.statusCode)
+		.send(ERROR_404.ROUTER_NOT_FOUND);
 });
 
-app.setErrorHandler((error: FastifyError, request: FastifyRequest, reply: FastifyReply): void => {
-    appLogger.info("setErrorHandler | url: ", request.url);
-    appLogger.error("setErrorHandler | error: ", error);
-    reply.status(ERROR_500.UNKNOWN.statusCode).send(ERROR_500.UNKNOWN);
-});
+app.setErrorHandler(
+	(error: FastifyError, request: FastifyRequest, reply: FastifyReply): void => {
+		appLogger.info("setErrorHandler | url: ", request.url);
+		appLogger.error("setErrorHandler | error: ", error);
+		reply.status(ERROR_500.UNKNOWN.statusCode).send(ERROR_500.UNKNOWN);
+	}
+);
 
 export default app;
--- app/index.ts
+++ app/index.ts
@@ -1,12 +1,12 @@
 import app from "./app";
-import {NUMBER} from "./src/common/constants/app.constants";
+import { NUMBER } from "./src/common/constants/app.constants";
 import ENV from "./src/common/env";
 import appLogger from "./src/common/logger";
 
-app.listen({ port: ENV.APP_PORT}, (error: Error): void => {
+app.listen({ port: ENV.APP_PORT }, (error: Error): void => {
 	if (error) {
 		appLogger.info(`App error:`, error);
 		process.exit(NUMBER.ONE);
 	}
-	appLogger.info(`App running on http://localhost:${process.env.APP_PORT}`);
+	appLogger.info(`App running on http://localhost:${ENV.APP_PORT}`);
 });
